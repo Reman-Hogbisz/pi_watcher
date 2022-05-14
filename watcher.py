d14306def706d2bd88cfab8c1e49fb11ea2ad903
Samuel Rembisz <sjrembisz07@gmail.com>
2022-05-14 19:34:28 -0400

    Added error logging

--- watcher.py
+++ watcher.py
@@ -1,14 +1,21 @@
 import requests
 import feedparser
 import os
+import sys
 from datetime import datetime, timedelta
 from time import sleep, mktime
+import pprint
 
+pp = pprint.PrettyPrinter(indent=4)
 
 RSS_URL = None
 WEBHOOK_URL = None
 
 LAST_CHECKED = datetime.now() - timedelta(hours=1)
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
 def filter_entry(entry):
@@ -18,21 +25,42 @@ def filter_entry(entry):
 
 def check_url():
     feed_response = feedparser.parse(RSS_URL)
-    last_built = datetime.fromtimestamp(
-        mktime(feed_response['feed']['updated_parsed']))
+    try:
+        last_built = datetime.fromtimestamp(
+            mktime(feed_response['feed']['updated_parsed']))
+    except KeyError:
+        eprint('No updated_parsed key in feed')
+        pp.print(feed_response)
+        return
+
     if last_built < LAST_CHECKED:
         print("[-] No new posts")
         return
 
-    entries = list(filter(filter_entry, feed_response['entries']))
+    try:
+        entries = list(filter(filter_entry, feed_response['entries']))
+    except KeyError:
+        eprint('No entries key in feed')
+        pp.print(feed_response)
+        return
     length_of_entries = len(entries)
 
     if length_of_entries > 0:
         print(f"[+] New posts found (found {length_of_entries})")
 
     for entry in entries:
-        title = entry['title']
-        link = entry['link']
+        try:
+            title = entry['title']
+        except KeyError:
+            eprint('No title in entry')
+            pp.print(entry)
+            continue
+        try:
+            link = entry['link']
+        except KeyError:
+            eprint('No link in entry')
+            pp.print(entry)
+            continue
         print(f"\t[+] Got new entry: {title}\n\t\t{link}")
         requests.post(WEBHOOK_URL, json={
             "content": f"{title}\n{link}"})
